c37d7068d1392a617429c7c53e0bc1edc0d8f139
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-03-20 16:18:30 +0530

    single post page updated

--- next/src/app/blog/[slug]/page.jsx
+++ next/src/app/blog/[slug]/page.jsx
@@ -1,6 +1,41 @@
+import Image from "next/image"
+import styles from "./singlePost.module.css"
+
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src=""
+          alt=""
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image 
+            className={styles.avatar}
+            src=""
+            alt=""
+            width={50}
+            height={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Subh B</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>published</span>
+            <span className={styles.detailValue}>20-03-24</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quibusdam dolorum numquam, deserunt quae incidunt.
+        </div>
+      </div>
+    </div>
   )
 }
 
