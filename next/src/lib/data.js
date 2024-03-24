8a5ea04d391ea8261e5df0fa147fbb91bb4daf54
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-03-24 18:27:06 +0530

    dbConnection and model

--- next/src/lib/data.js
+++ next/src/lib/data.js
@@ -3,7 +3,6 @@ const users = [
   { id: 1, name: "John" },
   { id: 2, name: "Jane" },
 ];
-
 const posts = [
   { id: 1, title: "Post 1", body: "......", userId: 1 },
   { id: 2, title: "Post 2", body: "......", userId: 1 },
