e3fa394cc0857e712e97ff117456d54f1a074f66
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-03-26 20:44:18 +0530

    add and delete server action

--- next/src/lib/action.js
+++ next/src/lib/action.js
@@ -1,4 +1,45 @@
-export const seyHello = async () => {
+import { Post } from "./models";
+import { connectDb } from "./utils";
+
+export const addPost= async (formData) => {
     "use server"
-    console.log("hello")
+
+    const { title, desc, slug, userId } = Object.fromEntries(formData);
+
+    try {
+        connectDb();
+        const newPost = new Post({
+          title,
+          desc,
+          slug,
+          userId,
+        })
+
+        await newPost.save();
+        console.log("saved to db");
+
+        revalidatePath("/blog");
+
+      } catch (err) {
+        console.log(err);
+        return { error: "Something went wrong!" };
+      }
+
+    console.log(title, desc, slug, userId)
+}
+
+export const deletePost = async(formData) => {
+    const {id} = Object.fromEntries(formData)
+
+    try {
+        connectDb();
+        await Post.findByIdAndDelete(id)
+        console.log("deleted from db")
+
+        revalidatePath("/blog")
+        
+    } catch (error) {
+        console.log(err);
+        return { error: "Something went wrong!" };
+    }
 }
